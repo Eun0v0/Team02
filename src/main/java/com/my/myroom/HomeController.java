cf2f746debaffba1b62e5e4a4cc3ba49e353ba4e
user <user@LAPTOP-D9RPCP7I>
2020-03-19 09:10:11 +0900

    create a package named 'com.my.dao'

--- src/main/java/com/my/myroom/HomeController.java
+++ src/main/java/com/my/myroom/HomeController.java
@@ -11,9 +11,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Handles requests for the application home page.
  */
+
+@Slf4j
 @Controller
 public class HomeController {
 	
@@ -24,7 +28,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		log.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
